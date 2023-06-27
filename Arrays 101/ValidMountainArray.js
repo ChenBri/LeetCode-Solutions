73170e70712355a92672addd447f0859190c7a16
ChenBr <chenbrilling1@gmail.com>
2023-06-28 01:16:17 +0300

    Updated mountain solution

--- Arrays 101/ValidMountainArray.js
+++ Arrays 101/ValidMountainArray.js
@@ -3,18 +3,17 @@
  * @return {boolean}
  */
 var validMountainArray = function (arr) {
-  let peak = true;
-
-  if (arr.length < 3 || arr[0] >= arr[1]) return false;
-
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] == arr[i + 1]) return false;
-
-    if (peak && arr[i] > arr[i + 1]) {
-      peak = false;
-    } else if (!peak && arr[i] <= arr[i + 1]) {
-      return false;
-    }
-  }
-  return !peak;
+    peak = false
+    
+    if(arr.length < 3 || arr[0]>=arr[1]) return false;
+    
+    for(let i=0; i<arr.length-1; i++){
+        if(arr[i]==arr[i+1]) return false;
+        
+        if(!peak && arr[i] > arr[i+1]) {
+            peak = true
+        } else if (peak && arr[i] < arr[i+1]) {
+            return false
+        }
+    } return peak;
 };
